fc139cb6f316e0289205fbd69add729f912cf2fb
Omar Soliman <omaarsolliiman@gmail.com>
2023-05-21 12:02:43 +0300

    fffff

---
+++ yamuorColors.dart
@@ -0,0 +1,108 @@
+import 'dart:ui';
+
+class YamuorColors {
+
+    /*--------------------- Custom colors ----------------------*/
+
+    static const Color cRed = Color(0xFFe1645a);
+    static const Color cGreen = Color(0xFF505a50);
+    static const Color cYellow = Color(0xFFf5e1b4);
+    static const Color cWhite = Color(0xFFf5f5f5);
+    static const Color cBlack = Color(0xFF000000);
+
+    /*--------------------- Light colors ----------------------*/
+
+    static const Color lightRed = Color(0xFFff3228);
+    static const Color lightOrange = Color(0xFFf58c05);
+    static const Color lightYellow = Color(0xFFf5c300);
+    static const Color lightGreen = Color(0xFF1ec337);
+    static const Color lightMint = Color(0xFFff3228);
+    static const Color lightTeal = Color(0xFF2da5be);
+    static const Color lightCyan = Color(0xFF41afdc);
+    static const Color lightBlue = Color(0xFF0064f5);
+    static const Color lightIndigo = Color(0xFF5550cd);
+    static const Color lightPurple = Color(0xFFa04bc8);
+    static const Color lightPink = Color(0xFFf5234b);
+    static const Color lightBrown = Color(0xFF967855);
+
+    /*--------------------- Dark colors ----------------------*/
+
+    static const Color darkRed = Color(0xFFff5046);
+    static const Color darkOrange = Color(0xFFffaa14);
+    static const Color darkYellow = Color(0xFFffe114);
+    static const Color darkGreen = Color(0xFF3ce155);
+    static const Color darkMint = Color(0xFF6ee1dc);
+    static const Color darkTeal = Color(0xFF46d2eb);
+    static const Color darkCyan = Color(0xFF5acdfa);
+    static const Color darkBlue = Color(0xFF148cff);
+    static const Color darkIndigo = Color(0xFF6464f0);
+    static const Color darkPurple = Color(0xFFcd64ff);
+    static const Color darkPink = Color(0xFFff4169);
+    static const Color darkBrown = Color(0xFFb49673);
+
+    /*--------------------- Accessible Light colors ----------------------*/
+
+    static const Color aLightRed = Color(0xFFc30519);
+    static const Color aLightOrange = Color(0xFFaf3c00);
+    static const Color aLightYellow = Color(0xFF915001);
+    static const Color aLightGreen = Color(0xFF006e19);
+    static const Color aLightMint = Color(0xFF0a7373);
+    static const Color aLightTeal = Color(0xFF01788c);
+    static const Color aLightCyan = Color(0xFF016996);
+    static const Color aLightBlue = Color(0xFF0041dc);
+    static const Color aLightIndigo = Color(0xFF3732a5);
+    static const Color aLightPurple = Color(0xFFaa46aa);
+    static const Color aLightPink = Color(0xFFc30f32);
+    static const Color aLightBrown = Color(0xFF785f3c);
+
+    /*--------------------- Accessible Dark colors ----------------------*/
+
+    static const Color aDarkRed = Color(0xFFff4137);
+    static const Color aDarkOrange = Color(0xFFffb441);
+    static const Color aDarkYellow = Color(0xFFffd228);
+    static const Color aDarkGreen = Color(0xFF32dc4b);
+    static const Color aDarkMint = Color(0xFF69d2d2);
+    static const Color aDarkTeal = Color(0xFF5fe6ff);
+    static const Color aDarkCyan = Color(0xFF6ed7ff);
+    static const Color aDarkBlue = Color(0xFF419bff);
+    static const Color aDarkIndigo = Color(0xFF7d78ff);
+    static const Color aDarkPurple = Color(0xFFdc91ff);
+    static const Color aDarkPink = Color(0xFFff3c5f);
+    static const Color aDarkBrown = Color(0xFFb49669);
+
+    /*--------------------- Light Grays ----------------------*/
+
+    static const Color  lightGray1 = Color(0xFF8c8c91);
+    static const Color  lightGray2 = Color(0xFFafafb4);
+    static const Color  lightGray3 = Color(0xFFc8c8cd);
+    static const Color  lightGray4 = Color(0xFFd2d2d7);
+    static const Color  lightGray5 = Color(0xFFe6e6eb);
+    static const Color  lightGray6 = Color(0xFFf0f0f5);
+
+    /*--------------------- Dark Grays ----------------------*/
+
+    static const Color darkGray1 = Color(0xFF8c8c91);
+    static const Color darkGray2 = Color(0xFF646464);
+    static const Color darkGray3 = Color(0xFF46464b);
+    static const Color darkGray4 = Color(0xFF3c3c3c);
+    static const Color darkGray5 = Color(0xFF2d2d2d);
+    static const Color darkGray6 = Color(0xFF1e1e1e);
+
+    /*--------------------- Accessible Light colors ----------------------*/
+
+    static const Color alightGray1 = Color(0xFF6e6e6e);
+    static const Color alightGray2 = Color(0xFF8c8c91);
+    static const Color alightGray3 = Color(0xFFafafb4);
+    static const Color alightGray4 = Color(0xFFbebebe);
+    static const Color alightGray5 = Color(0xFFd7d7dc);
+    static const Color alightGray6 = Color(0xFFebebf0);
+
+    /*--------------------- Accessible Dark colors ----------------------*/
+
+    static const Color adarkGray1 = Color(0xFFafafb4);
+    static const Color adarkGray2 = Color(0xFF7d7d82);
+    static const Color adarkGray3 = Color(0xFF555555);
+    static const Color adarkGray4 = Color(0xFF464646);
+    static const Color adarkGray5 = Color(0xFF373737);
+    static const Color adarkGray6 = Color(0xFF242428);
+}
